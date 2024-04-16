55dd4ee128aada4650a0614d2bc79f142b328da5
DESKTOP-MBRCU6K\Aleksandr <sk@vin.ua>
2024-04-16 18:59:09 +0300

    [Refactor] changed name anchor to Anchor with capital letter in start

--- Assets/Scripts/GameBoardCell.cs
+++ Assets/Scripts/GameBoardCell.cs
@@ -1,9 +1,10 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class GameBoardCell : MonoBehaviour
 {
-    public Transform anchor;
+    public Transform Anchor;
     public Checker PlacedChecker { get; private set; }
     public bool HasRisenPlacedObject { get; private set; }
     public bool IsEmpty => PlacedChecker == null;
@@ -14,10 +15,10 @@ public class GameBoardCell : MonoBehaviour
         Position = position;
         gameObject.name += $"X:{position.x},Y:{position.y}";
     }
-    
+
     public void Place(Checker checker)
     {
-        checker.transform.position = anchor.position;// + Vector3.up * (checker.Height * 0.5f);
+        checker.transform.position = Anchor.position;
         PlacedChecker = checker;
     }
 
@@ -30,21 +31,21 @@ public class GameBoardCell : MonoBehaviour
     public void RisePlacedObject()
     {
         if (PlacedChecker == null) return;
-        
+
         var position = PlacedChecker.transform.position;
-        PlacedChecker.transform.DOMove(new Vector3(position.x, anchor.transform.position.y + 0.5f, position.z), 0.5f);
+        PlacedChecker.transform.DOMove(new Vector3(position.x, Anchor.transform.position.y + 0.5f, position.z), 0.5f);
         HasRisenPlacedObject = true;
     }
-    
+
     public void MovePlacedObjectToGround()
     {
         if (PlacedChecker == null) return;
-        
+
         var position = PlacedChecker.transform.position;
         DoTweenMovement(position);
         HasRisenPlacedObject = false;
     }
-    
+
     public void ChangeColor(GameObject prefab, Color color)
     {
         var rend = prefab.GetComponent<Renderer>();
@@ -53,6 +54,6 @@ public class GameBoardCell : MonoBehaviour
 
     private void DoTweenMovement(Vector3 position)
     {
-        PlacedChecker.transform.DOMove(new Vector3(position.x, anchor.transform.position.y, position.z), 0.5f);
+        PlacedChecker.transform.DOMove(new Vector3(position.x, Anchor.transform.position.y, position.z), 0.5f);
     }
 }
