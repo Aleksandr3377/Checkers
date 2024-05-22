8e911efacc47964b2519c9e76bd995f486f7e821
DESKTOP-MBRCU6K\Aleksandr <sk@vin.ua>
2024-05-22 20:07:43 +0300

    added and transformed a few objects

--- Assets/Scripts/Camera/MouseRotationControl.cs
+++ Assets/Scripts/Camera/MouseRotationControl.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class MouseRotationControl : OrbitControlBase
 {
-    private readonly float _maxRotationX = 120f;
-    
+    [SerializeField] private float _maxRotationX = 120f;
+
     protected override float GetDeltaRotX()
     {
-        var deltaRotX= GetMouseDelta().y * RotationSpeed;
+        var deltaRotX = GetMouseDelta().y * RotationSpeed;
         var nextRotX = _finalRotX + deltaRotX;
         if (nextRotX > _maxRotationX)
         {
@@ -19,15 +19,15 @@ public class MouseRotationControl : OrbitControlBase
 
         return deltaRotX;
     }
-    
+
     protected override float GetDeltaRotY()
     {
         return GetMouseDelta().x * RotationSpeed;
     }
-    
+
     private Vector2 GetMouseDelta()
     {
-        if (!Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.touchCount>1)
+        if (!Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.touchCount > 1)
         {
             return Vector2.zero;
         }
